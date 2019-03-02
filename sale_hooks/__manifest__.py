5c71956a90bd0e16a9caade8270945bf3eb71101
odooerpdevelopers <odooerpdevelopers@gmail.com>
2019-03-02 20:13:21 +0100

    Add example hooks module

---
+++ sale_hooks/__manifest__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "SaleHooks",
+
+    'summary': """
+        Odoo Hooks Examples """,
+
+    'description': """
+        Educational Module Odoo Hooks Example
+    """,
+
+    'author': "odooerpcloud",
+    'website': "https://www.odooerpcloud.com",
+
+    'category': 'Sales',
+    'version': '0.1',
+
+    'depends': ['sale_management'],
+
+    # always loaded
+    'data': [],
+
+    # 'pre_init_hook': "_pre_init_hook",
+    # 'post_init_hook': "_post_init_hook",
+    # 'uninstall_hook': "_uninstall_hook",
+}
---
+++ sale_hooks/hooks.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019 odooerpcloud.com
+
+
+def _pre_init_hook(cr):
+    pass
+
+
+def _post_init_hook(cr, registry):
+    pass
+
+
+def _uninstall_hook(cr, registry):
+    pass
